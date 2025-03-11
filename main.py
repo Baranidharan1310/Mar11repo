5f3b6be0d341d15cb832bcd499fb140253c54930
Baranidharan1310 <baranidharan1320@gmail.com>
2025-03-11 21:54:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # main.py
 from flask import Flask
 
-# Create the Flask app
 app = Flask(__name__)
 
-# Define a route for the root URL
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-# Run the app (for local testing)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
